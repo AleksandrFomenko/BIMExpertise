a4d2474af7155f51cf4b38a4a60f49a62037fe7b
Your Name <umr228@gmail.com>
2025-02-16 03:07:02 +0300

    Первый коммит

---
+++ BIMExpertise/Application.cs
@@ -0,0 +1,25 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using BIMExpertise.Commands;
+
+namespace BIMExpertise;
+
+/// <summary>
+///     Application entry point
+/// </summary>
+[UsedImplicitly]
+public class Application : ExternalApplication
+{
+    public override void OnStartup()
+    {
+        CreateRibbon();
+    }
+
+    private void CreateRibbon()
+    {
+        var panel = Application.CreatePanel("Commands", "BIMExpertise");
+
+        panel.AddPushButton<StartupCommand>("Execute")
+            .SetImage("/BIMExpertise;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/BIMExpertise;component/Resources/Icons/RibbonIcon32.png");
+    }
+}
